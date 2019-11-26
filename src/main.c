755b89bf33d8824ce681b9115f25c4c3dad7991b
Brobro60 <cailliez.brody@gmail.com>
2019-11-26 22:21:20 +0100

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char s[1024];
+
+	while (scanf("%s", &s) != EOF)
+	{
+		//
+		// Lisez les données ici et effectuez votre traitement ici
+		//
+	}
+
+	// Vous pouvez aussi effectuer votre traitement ici après avoir lu toutes les données
+	return 0;
+}
